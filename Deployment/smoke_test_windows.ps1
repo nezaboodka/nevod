57577d302ad0fb2c2046662f3c98ce911440af35
Ivan Shimko <vana.shimko@gmail.com>
2021-06-08 08:40:26 +0300

    test-windows-zip-package: call negrep from current directory

--- Deployment/smoke_test_windows.ps1
+++ Deployment/smoke_test_windows.ps1
@@ -25,8 +25,8 @@ function Test-WindowsZipPackage {
     cat ./NOTICE
     cat ./LICENSE.txt
     cat ./THIRD-PARTY-NOTICES.txt
-    Invoke-NativeCommand negrep -f ./examples/patterns.np ./examples/example.txt
-    Invoke-NativeCommand negrep --version
+    Invoke-NativeCommand ./negrep.exe -f ./examples/patterns.np ./examples/example.txt
+    Invoke-NativeCommand ./negrep.exe --version
 }
 
 function Invoke-NativeCommand() {
